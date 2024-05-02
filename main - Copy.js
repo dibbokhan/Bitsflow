84273ae8a186ed2f34e547df1105bc6f8a14ca90
Dibbo Khan <113321679+dibbokhan@users.noreply.github.com>
2024-05-02 16:24:42 +0600

    Delete main - Copy.js

--- main - Copy.js
+++
@@ -1,9 +0,0 @@
-const first = document.querySelector(".1st");
-
-const second = document.querySelector(".2nd");
-
-
-
-first.addEventListener("mouseover", () => {
-    second.style.display = "none";
-})
